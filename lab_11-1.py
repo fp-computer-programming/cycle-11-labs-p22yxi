65398aaec1d3dc75e6d741b994f1051339661e2e
p22yxi <p22yxi@fairfieldprep.org>
2022-02-08 10:19:09 -0500

    1,2

---
+++ lab_11-1.py
@@ -0,0 +1,8 @@
+# Yongdong Xi
+my_film = open("alma_mater.txt")
+contents = my_film.read()
+
+my_film.close()
+
+print(contents)
+
---
+++ lab_11-2.py
@@ -0,0 +1,11 @@
+# Yongdong Xi
+my_film = open("alma_mater.txt",'r')
+contents = my_film.readlines()
+
+my_film.close()
+
+
+print(contents[::-1])
+
+
+
